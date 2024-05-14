2c211a7cefe1fb2213c8c4b33e89cd0e0a603d25
iagof5 <162588542+iagof5@users.noreply.github.com>
2024-05-15 00:16:54 +0200

    Update Cliente.java
    comentarios clase cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,7 +1,7 @@
-public class Cliente {
-    private String nombre;
-    private String telefono;
-    private String fechaHora;
+public class Cliente {//declaramos unha clase cliente para poder introducir os seus datos.
+    private String nombre;//declaramos nombre.
+    private String telefono;//declaramos telefono.
+    private String fechaHora;//declaramos fecha e hora.
 
     public Cliente(String nombre, String telefono, String fechaHora) {
         this.nombre = nombre;
