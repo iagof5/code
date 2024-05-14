8da3d80f7bd71a29ceeabdeb10d2ba3ec36929fd
iagof5 <162588542+iagof5@users.noreply.github.com>
2024-05-15 00:15:31 +0200

    Update Barberia.java
    comentarios en clase barberia

--- src/Barberia.java
+++ src/Barberia.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 
-public class Barberia {
+public class Barberia {//declaramos a clase barberia na cal se van gardar as reservas.
     private ArrayList<Cliente> listaReservas;
 
     public Barberia() {
-        listaReservas = new ArrayList<>();
+        listaReservas = new ArrayList<>();//creamos un arraylist xa que non sabemos cantos clientes reservaran, eso cun array normal seria complicado xa que ocuparia demasiado espacio.
     }
 
     public void hacerReserva(Cliente cliente) {
@@ -12,9 +12,9 @@ public class Barberia {
         System.out.println("Reserva realizada para " + cliente.getNombre() + " el " + cliente.getFechaHora());
     }
 
-    public void mostrarReservas() {
+    public void mostrarReservas() {//aqui mostraranse as reservas ao pulsar o numero 2.
         if (listaReservas.isEmpty()) {
-            System.out.println("No hay reservas.");
+            System.out.println("No hay reservas.");//en caso de non ter reservas o programa faranolo saber.
         } else {
             System.out.println("Reservas:");
             for (Cliente cliente : listaReservas) {
