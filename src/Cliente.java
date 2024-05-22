2757b440bfc65966ff897f8dbd1f5c9a4dfee152
iagof5 <162588542+iagof5@users.noreply.github.com>
2024-05-22 19:30:40 +0200

    Update Cliente.java
    comentarios javadoc cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,22 +1,47 @@
-public class Cliente {//declaramos unha clase cliente para poder introducir os seus datos.
-    private String nombre;//declaramos nombre.
-    private String telefono;//declaramos telefono.
-    private String fechaHora;//declaramos fecha e hora.
+/**
+ * Clase que representa un cliente de la barbería.
+ */
+class Cliente {
+    private String nombre;
+    private String telefono
+    private String fechaHora;
 
+    /**
+     * Constructor que inicializa los datos del cliente.
+     *
+     * @param nombre    El nombre del cliente.
+     * @param telefono  El teléfono del cliente.
+     * @param fechaHora La fecha y hora de la reserva.
+     */
     public Cliente(String nombre, String telefono, String fechaHora) {
         this.nombre = nombre;
         this.telefono = telefono;
         this.fechaHora = fechaHora;
     }
 
+    /**
+     * Obtiene el nombre del cliente.
+     *
+     * @return El nombre del cliente.
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Obtiene el teléfono del cliente.
+     *
+     * @return El teléfono del cliente.
+     */
     public String getTelefono() {
         return telefono;
     }
 
+    /**
+     * Obtiene la fecha y hora de la reserva del cliente.
+     *
+     * @return La fecha y hora de la reserva.
+     */
     public String getFechaHora() {
         return fechaHora;
     }
